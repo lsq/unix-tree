3a860d602b2a554718d897d0d5cfc8d68ae8be0e
lsq <lsqypj.gmail.com>
2025-05-06 22:19:14 +0800

    fix: gethostname

--- tree.h
+++ tree.h
@@ -43,6 +43,7 @@
 #include "mingw-w64/include/grp.h"
 #include "mingw-w64/include/langinfo.h"
 #include "mingw-w64/include/unistd.h"
+#include <winsock2.h>
 #endif
 /******** end of adapt for mingw-w64*/
 #ifdef __EMX__  /* for OS/2 systems */
